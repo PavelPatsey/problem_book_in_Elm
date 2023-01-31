1a9401d35a56ba2fc3f8687a1772519db71fb42a
Павел Пацей <patsey.pavel@gmail.com>
2023-01-31 22:13:31 +0300

    solve begin_03 with two funcions

---
+++ 03_begin/src/Begin_03_v3_two_functions.elm
@@ -0,0 +1,100 @@
+-- Begin3°. Даны стороны прямоугольника a и b. Найти его площадь S = a·b и
+-- периметр P = 2·(a + b).
+
+
+module Begin_03_v3_two_functions exposing (..)
+
+import Browser
+import Html exposing (Html, div, input, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+
+
+-- MAIN
+
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { a : String
+    , b : String
+    }
+
+
+init : Model
+init =
+    { a = ""
+    , b = ""
+    }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Change_a String
+    | Change_b String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Change_a new_a ->
+            { model | a = new_a }
+
+        Change_b new_b ->
+            { model | b = new_b }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ div [] [ text "Сторона прямоугольника а = " ]
+        , input [ placeholder "а = ", value model.a, onInput Change_a ] []
+        , div [] [ text "Сторона прямоугольника b = " ]
+        , input [ placeholder "b = ", value model.b, onInput Change_b ] []
+        , div [] [ text "Площадь прямоугольника S = " ]
+        , div []
+            [ text
+                (String.fromInt
+                    (calculate_area
+                        ( Maybe.withDefault 0 (String.toInt model.a)
+                        , Maybe.withDefault 0 (String.toInt model.b)
+                        )
+                    )
+                )
+            ]
+        , div [] [ text "Периметр прямоугольника p = " ]
+        , div []
+            [ text
+                (String.fromInt
+                    (calculate_perimeter
+                        ( Maybe.withDefault 0 (String.toInt model.a)
+                        , Maybe.withDefault 0 (String.toInt model.b)
+                        )
+                    )
+                )
+            ]
+        ]
+
+
+calculate_perimeter : ( Int, Int ) -> Int
+calculate_perimeter ( a, b ) =
+    2 * (a + b)
+
+
+calculate_area : ( Int, Int ) -> Int
+calculate_area ( a, b ) =
+    a * b
