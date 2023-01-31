155c5828b8f2745efd67310b97234e7de86f3677
Павел Пацей <patsey.pavel@gmail.com>
2023-01-31 21:52:56 +0300

    solve begin_03

--- 03_begin/src/Begin_03.elm
+++ 03_begin/src/Begin_03.elm
@@ -1,3 +1,6 @@
+-- Begin3°. Даны стороны прямоугольника a и b. Найти его площадь S = a·b и
+-- периметр P = 2·(a + b).
+
 module Begin_03 exposing (..)
 
 import Browser
@@ -60,14 +63,20 @@ view model =
     , input [ placeholder "а = ", value model.a, onInput Change_a ] []
     , div [] [ text "Сторона прямоугольника b = " ]
     , input [ placeholder "b = ", value model.b, onInput Change_b ] []
-    , div [] [ text "Площадь квадрата S = " ]
-    , div [] [ text (String.fromInt Tuple.first (begin_03 
-      (Maybe.withDefault 0 ( String.toInt model.a)), (Maybe.withDefault 0 ( String.toInt model.b)) )) ]
+    , div [] [ text "Площадь прямоугольника S = " ]
+    , div [] [ text (String.fromInt (Tuple.first (begin_03 
+      (Maybe.withDefault 0 ( String.toInt model.a)
+       , Maybe.withDefault 0 ( String.toInt model.b)
+      ))))
+      ]
+    , div [] [ text "Периметр прямоугольника p = " ]
+    , div [] [ text (String.fromInt (Tuple.second (begin_03 
+      (Maybe.withDefault 0 ( String.toInt model.a)
+       , Maybe.withDefault 0 ( String.toInt model.b)
+      ))))
+      ]
     ]
 
 begin_03 : (Int, Int) -> (Int, Int)
 begin_03 (a, b) =
-  (a * 2, 2 * (a + b))
-
--- Begin3°. Даны стороны прямоугольника a и b. Найти его площадь S = a·b и
--- периметр P = 2·(a + b).
+  (a * b, 2 * (a + b))
