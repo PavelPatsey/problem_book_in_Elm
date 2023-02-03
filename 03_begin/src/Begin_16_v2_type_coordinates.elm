a84bf9c75c2a02ac3d6d934787a7b8f85505ebc8
Павел Пацей <patsey.pavel@gmail.com>
2023-02-03 22:48:19 +0300

    modified:   Begin_16_v1.elm

--- 03_begin/src/Begin_16_v2_type_coordinates.elm
+++ 03_begin/src/Begin_16_v2_type_coordinates.elm
@@ -1,4 +1,4 @@
-module Begin_16_v1 exposing (..)
+module Begin_16 exposing (..)
 
 import Browser
 import Html exposing (Html, div, input, text)
@@ -19,16 +19,20 @@ main =
 -- MODEL
 
 
-type alias Model =
-    { x1 : String
-    , x2 : String
+type alias Coordinates =
+    { x1 : Float
+    , x2 : Float
     }
+
+
+type alias Model =
+    Coordinates
 
 
 init : Model
 init =
-    { x1 = ""
-    , x2 = ""
+    { x1 = 0
+    , x2 = 0
     }
 
 
@@ -45,10 +49,10 @@ update : Msg -> Model -> Model
 update msg model =
     case msg of
         Change_x1 new_x1 ->
-            { model | x1 = new_x1 }
+            { model | x1 = Maybe.withDefault 0 (String.toFloat new_x1) }
 
         Change_x2 new_x2 ->
-            { model | x2 = new_x2 }
+            { model | x2 = Maybe.withDefault 0 (String.toFloat new_x2) }
 
 
 
@@ -59,17 +63,25 @@ view : Model -> Html Msg
 view model =
     div []
         [ div [] [ text "Значение координаты x1: " ]
-        , input [ placeholder "x1 = ", value model.x1, onInput Change_x1 ] []
+        , input
+            [ placeholder "x1 = "
+            , value (String.fromFloat model.x1)
+            , onInput Change_x1
+            ]
+            []
         , div [] [ text "Значение координаты x2: " ]
-        , input [ placeholder "x2 = ", value model.x2, onInput Change_x2 ] []
+        , input
+            [ placeholder "x2 = "
+            , value (String.fromFloat model.x2)
+            , onInput Change_x2
+            ]
+            []
         , div [] [ text "Расстояние между двумя точками x1 и x2 = " ]
         , div []
             [ text
                 (String.fromFloat
                     (abs
-                        (Maybe.withDefault 0 (String.toFloat model.x1)
-                            - Maybe.withDefault 0 (String.toFloat model.x2)
-                        )
+                        (model.x1 - model.x2)
                     )
                 )
             ]
@@ -79,3 +91,4 @@ view model =
 
 -- Begin16. Найти расстояние между двумя точками с заданными координатами
 -- x1 и x2 на числовой оси: |x2 – x1|.
+-- !!!В модели Float, поэтому нельзя вводить в placeholder String.
