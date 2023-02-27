c1837f28e5bdbd6d9f1a8c488ad3215cef68d74f
Павел Пацей <patsey.pavel@gmail.com>
2023-02-27 23:02:34 +0300

    solve Begin_22_v2_ChangeAB_from_initial_values.elm

--- 03_begin/src/Begin_22/Begin_22_v2_ChangeAB_from_initial_values.elm
+++ 03_begin/src/Begin_22/Begin_22_v2_ChangeAB_from_initial_values.elm
@@ -1,9 +1,9 @@
 module Begin_22.Begin_22_v2_ChangeAB_from_initial_values exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, input, text)
+import Html exposing (Html, button, div, text)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick, onInput)
+import Html.Events exposing (onClick)
 
 
 
@@ -24,16 +24,10 @@ type alias Model =
     }
 
 
-type alias Booster =
-    { a : String
-    , b : String
-    }
-
-
 init : Model
 init =
-    { a = 0
-    , b = 0
+    { a = 2
+    , b = 5
     }
 
 
@@ -43,8 +37,6 @@ init =
 
 type Msg
     = ChangeAB
-    | ChangeA String
-    | ChangeB String
 
 
 update : Msg -> Model -> Model
@@ -52,12 +44,6 @@ update msg model =
     case msg of
         ChangeAB ->
             { model | a = model.b, b = model.a }
-
-        ChangeA newA ->
-            { model | a = Maybe.withDefault 0 (String.toFloat newA) }
-
-        ChangeB newB ->
-            { model | b = Maybe.withDefault 0 (String.toFloat newB) }
 
 
 
@@ -67,32 +53,10 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ div []
-            [ input
-                [ placeholder "A = ", value (String.fromFloat model.a) ]
-                []
-            ]
-        , div []
-            [ input [ placeholder "B = ", value (String.fromFloat model.b) ] []
-            ]
-        , button [ onClick Change ] [ text "Поменять местами содержимое переменных A и B" ]
-        , div [] [ text ("A = " ++ String.fromFloat model.a) ]
+        [ div [] [ text ("A = " ++ String.fromFloat model.a) ]
         , div [] [ text ("B = " ++ String.fromFloat model.b) ]
+        , button [ onClick ChangeAB ] [ text "Поменять местами содержимое переменных A и B" ]
         ]
-view : Model -> Html Msg
-view model =
-    div []
-        [ viewInput "text" "A = " model.a a
-        , viewInput "text" "B = " model.b a
-        , button [ onClick Change ] [ text "Поменять местами содержимое переменных A и B" ]
-        , div [] [ text ("A = " ++ String.fromFloat model.a) ]
-        , div [] [ text ("B = " ++ String.fromFloat model.b) ]
-        ]
-
-
-viewInput : String -> String -> String -> (String -> msg) -> Html msg
-viewInput t p v toMsg =
-    input [ type_ t, placeholder p, value v, onInput toMsg ] []
 
 
 
