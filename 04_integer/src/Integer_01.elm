b4ceb407d3218b3f82d5e4ac79a56748e362cf02
Павел Пацей <patsey.pavel@gmail.com>
2023-05-05 23:15:22 +0300

    elm-format

--- 04_integer/src/Integer_01.elm
+++ 04_integer/src/Integer_01.elm
@@ -1,7 +1,7 @@
 module Integer_01 exposing (..)
 
 import Browser
-import Html exposing (Html, Attribute, div, input, text)
+import Html exposing (Attribute, Html, div, input, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 
@@ -11,7 +11,7 @@ import Html.Events exposing (onInput)
 
 
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
 
 
 
@@ -19,15 +19,16 @@ main =
 
 
 type alias Model =
-  { content : String
-  , distance : Int
-  }
+    { content : String
+    , distance : Int
+    }
 
 
 init : Model
 init =
-  { content = "" 
-  , distance = 0}
+    { content = ""
+    , distance = 0
+    }
 
 
 
@@ -35,15 +36,17 @@ init =
 
 
 type Msg
-  = Change String
+    = Change String
 
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Change newContent ->
-      { model | content = newContent , distance = Maybe.withDefault 0 (String.toInt newContent)
-      }
+    case msg of
+        Change newContent ->
+            { model
+                | content = newContent
+                , distance = Maybe.withDefault 0 (String.toInt newContent)
+            }
 
 
 
@@ -52,19 +55,21 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ div [] [ text "Введите значения расстояния L в сантиметрах: " ]
-    , input [ placeholder "Значение L в сантиметрах", value model.content, onInput Change ] []
-    , div [] [ text ("Значение L в сантиметрах:") ]
-    , div [] [ text (String.fromInt model.distance) ]
-    , div [] [ text ("Количество полных метров в L:") ]
-    , div [] [ text (String.fromInt (getAnswer model.distance)) ]
-    ]
+    div []
+        [ div [] [ text "Введите значения расстояния L в сантиметрах: " ]
+        , input [ placeholder "Значение L в сантиметрах", value model.content, onInput Change ] []
+        , div [] [ text "Значение L в сантиметрах:" ]
+        , div [] [ text (String.fromInt model.distance) ]
+        , div [] [ text "Количество полных метров в L:" ]
+        , div [] [ text (String.fromInt (getAnswer model.distance)) ]
+        ]
 
-getAnswer: Int -> Int
-getAnswer distance = 
-  distance // 1000
 
-    
+getAnswer : Int -> Int
+getAnswer distance =
+    distance // 1000
+
+
+
 -- Integer1. Дано расстояние L в сантиметрах. Используя операцию деления
 -- нацело, найти количество полных метров в нем (1 метр = 100 см).
